375a5fea6ff76dc7fe2a7440f68f3132a3bde520
Kevin Avignon <kevin.avignon@hypertherm.com>
2020-01-06 16:05:35 -0500

    Added initial benchmarks project to measure expected perf gains.

---
+++ Robotmaster.CollectionRecommendation/Robotmaster.CollectionRecommendation.Benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Robotmaster.CollectionRecommendation.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
